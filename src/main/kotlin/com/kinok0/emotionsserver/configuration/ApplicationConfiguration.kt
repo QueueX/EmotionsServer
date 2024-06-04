07cedb68594466df85f6b86c1f0ff67b58418b7e
Kinok0 <truthpruthbg@gmail.com>
2024-06-04 22:17:43 +0300

    Test CORS config

--- src/main/kotlin/com/kinok0/emotionsserver/configuration/ApplicationConfiguration.kt
+++ src/main/kotlin/com/kinok0/emotionsserver/configuration/ApplicationConfiguration.kt
@@ -1,14 +1,13 @@
 package com.kinok0.emotionsserver.configuration
 
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
-import kotlin.random.Random
 
 @Configuration
-class ApplicationConfiguration {
-
-    @Bean
-    fun random() = Random
-
+class ApplicationConfiguration : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**").allowedMethods("*")
+    }
 }
